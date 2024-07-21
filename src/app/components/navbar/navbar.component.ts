cf8161f5a1c463b1688bee0a9cf659fb6cda6d74
bonolo2024 <bonolo@thedigitalacademy.co.za>
2024-07-21 21:40:48 +0200

    Navbar, New-employee and View-employee changes

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(private router: Router) {}
+
+  ngOnInit():void {
+
+  }
+
+  public getAllEmployees() {
+    this.router.navigate(['/']); 
+    // this.employeeService.getAllEmployees().subscribe(data => {this.employees = data})
+  }
 
 }
